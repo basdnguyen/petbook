36a7a4426fcfdf7b4cf6dc0760986dd367e8b596
Bas Nguyen <basdnguyen@gmail.com>
2024-07-09 15:35:49 +0700

    rework signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { VisibilityOff, Visibility } from "@mui/icons-material";
-import { Box, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField } from "@mui/material";
+import { Box, Button, Container, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, useTheme } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import axios from "axios";
 import { useFormik } from "formik";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { CldImage } from "next-cloudinary";
 
 interface FormData {
   first_name: string;
@@ -17,6 +19,7 @@ interface FormData {
 export default function SignUp() {
   const router = useRouter();
   const [isProcessing, setIsProcessing] = useState(false);
+  const { palette } = useTheme();
 
   const signUp = async (values: FormData) => {
     setIsProcessing(true);
@@ -44,84 +47,106 @@ export default function SignUp() {
   };
 
   return (
-    <form onSubmit={formik.handleSubmit} className='flex flex-1 flex-col justify-center'>
-      <Container maxWidth='sm' sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        backgroundColor: 'white', gap: 2, paddingY: 2,
-        justifyContent: 'center',
-      }}>
-        <TextField
-          required
-          id="first_name"
-          label="First name"
-          onChange={formik.handleChange}
-          value={formik.values.first_name}
+    <Grid2 container minHeight={'100vh'}>
+      <Grid2 flex={1} sx={{ backgroundColor: palette.background.default }} container alignItems={'center'}>
+        <form onSubmit={formik.handleSubmit} className='flex flex-1 flex-col justify-center'>
+          <Container maxWidth='sm' sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2, paddingY: 2,
+            justifyContent: 'center',
+          }}>
+            <Grid2 container gap={2}>
+              <TextField
+                required
+                id="first_name"
+                label="First name"
+                onChange={formik.handleChange}
+                value={formik.values.first_name}
+                className="flex-1"
+              />
+              <TextField
+                required
+                id="last_name"
+                label="Last name"
+                onChange={formik.handleChange}
+                value={formik.values.last_name}
+                className="flex-1"
+              />
+            </Grid2>
+            <TextField
+              required
+              id="email"
+              label="Email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            <Grid2 container gap={2}>
+            <FormControl className="flex-1">
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <OutlinedInput
+                required
+                id="password"
+                label="Password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl className="flex-1">
+              <InputLabel htmlFor="confirm_password">Confirm password</InputLabel>
+              <OutlinedInput
+                required
+                id="confirm_password"
+                label="Confirm password"
+                onChange={formik.handleChange}
+                value={formik.values.confirm_password}
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            </Grid2>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}>
+              <Button variant="contained" type="submit" disabled={isProcessing}>Sign up</Button>
+            </Box>
+          </Container>
+        </form>
+      </Grid2>
+      <Grid2 flex={1} position={'relative'}>
+        <CldImage
+          fill
+          crop='fill'
+          src="2150007407_ehjjnc"
+          sizes="100vw"
+          alt="pets"
+          style={{
+            objectFit: 'cover',
+          }}
         />
-        <TextField
-          required
-          id="last_name"
-          label="Last name"
-          onChange={formik.handleChange}
-          value={formik.values.last_name}
-        />
-        <TextField
-          required
-          id="email"
-          label="Email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <FormControl>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <OutlinedInput
-            required
-            id="password"
-            label="Password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="confirm_password">Confirm password</InputLabel>
-          <OutlinedInput
-            required
-            id="confirm_password"
-            label="Confirm password"
-            onChange={formik.handleChange}
-            value={formik.values.confirm_password}
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}>
-          <Button variant="contained" type="submit" disabled={isProcessing}>Sign up</Button>
-        </Box>
-      </Container>
-    </form>
+      </Grid2>
+    </Grid2>
   )
 }
